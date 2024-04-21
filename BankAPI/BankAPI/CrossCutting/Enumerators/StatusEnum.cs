57e1a513e9bfb55c4d78eb0afdbdbe947ec47c9d
Jair Guerrero <jaiguers@outlook.com>
2024-04-21 11:02:07 -0500

    feature: Agregar proyecto

---
+++ BankAPI/BankAPI/CrossCutting/Enumerators/StatusEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace BankAPI.CrossCutting.Enumerators
+{
+    public enum StatesEnum
+    {
+        Active = 1,
+        Inactive = 2,
+        Approved = 3,
+        Rejected = 4,
+        Review = 5,
+        Pending = 6,
+        Success = 7, 
+        Failure = 8,
+    }
+}
